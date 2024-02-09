6e69b945610671ba945ab9658887ce75081b1690
Dmitry Liapin <ldemetrios@yandex.ru>
2024-02-09 18:57:51 +0300

    Init

---
+++ build.gradle.kts
@@ -0,0 +1,63 @@
+import org.gradle.process.internal.ExecException
+import java.io.ByteArrayOutputStream
+
+operator fun String.invoke(vararg args: String): List<String> {
+    val out = ByteArrayOutputStream()
+//    val err = ByteArrayOutputStream()
+    val res = project.exec {
+        println(this@invoke + " " + args.joinToString(" "))
+        commandLine(this@invoke, *args)
+        standardOutput = out
+        errorOutput = System.err
+        workingDir = project.projectDir
+    }
+
+    return out.toString().split(Regex("\r\n|[\r\n\u2028\u2029\u0085]")).filter(String::isNotBlank)
+}
+
+val typst = "typst"
+
+tasks.register("typst-compile") {
+    group = "typst"
+    description = "Compiles all the typst files with all the available modes"
+    doLast {
+        val sources = ("find"(rootDir.path, "-name", "*.typ"))
+
+        val modefile = File("mode.txt")
+
+        var allSuccess = true
+
+        for (source in sources) {
+            val modes = try {
+                typst("query", source, "<available-modes>", "--field", "value")
+            } catch (e: ExecException) {
+                println("Can't query available modes for $source")
+                allSuccess = false
+                continue
+            }
+                .joinToString(" ")
+                .filter { it !in "[]" }
+                .split(",")
+                .filter(String::isNotBlank)
+                .map(String::trim)
+                .map { it.substring(1, it.length - 1) }
+
+
+            for (mode in modes) {
+                try {
+                    val modedir = "${rootDir}/output/$mode"
+                    File(modedir).mkdirs()
+                    modefile.writeText(mode)
+                    typst("c", source, "${rootDir}/output/$mode/" + File(source).nameWithoutExtension + ".pdf")
+                } catch (e: ExecException) {
+                    allSuccess = false
+                    println("Can't compile $source with mode $mode")
+                }
+            }
+        }
+
+        if(!allSuccess) {
+            throw AssertionError("Not all files were compiled")
+        }
+    }
+}
